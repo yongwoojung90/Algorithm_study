ff46dc94189d36496b865839d0b65d4fdb0d4c42
yongwoojung90 <loyyd03@gmail.com>
2015-07-12 17:29:57 +0900

    그래프 안썼고, 예외처리 안되서 우롱앤써. input2.txt.에서 5번째 케이스 예외처리 해야됨

--- GBSSM_7/problem7_2.c
+++ GBSSM_7/problem7_2.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
-
-
 //----------------------------------------------
 int arr[100][100] = { 0, };
 int n = 0;
+int check[100] = { 0, };
+int flag = 0;
 //----------------------------------------------
-
-
+void check_row(int row){
+	for (int j = 0; j < n; j++){
+		if (arr[row][j] == 0){ 
+			check[j] = 1; 
+			printf("%d ", j + 1);
+		}
+	}
+	printf("\n");
+}
+int invalid_check(){
+	if (n == 1){
+		return 0;
+	}
+	int cnt = 0;
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < n; j++){
+			if (arr[i][j] == 0){
+				cnt++;
+			}
+		}
+		if (cnt < 2){
+			return 0;
+		}
+		else{
+			cnt = 0;
+		}
+	}
+	return 1;
+}
 int main()
 {
 	int itr;
@@ -22,9 +49,24 @@ int main()
 				scanf("%d", &arr[i][j]);
 			}
 		}
-
+		if (0 == invalid_check()){
+			flag = 1;
+			printf("0\n");
+		}
+		if (flag == 0){
+			for (int i = 0; i < n; i++){
+				if (check[i] == 1) continue;
+					check_row(i);
+			}
+		}
 		/*init*/
-
+		for (int i = 0; i < n; i++){
+			for (int j = 0; j < n; j++){
+				arr[i][j] = 0;
+			}
+			check[i] = 0;
+		}
+		flag = 0;
 	}
 
 	return 0;
